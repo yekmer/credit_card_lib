c969c8c4ef71cfc1f9148bb846caddc10d704556
yekmer <yekmer@gmail.com>
2015-03-17 00:40:37 +0100

    textwatcher for expiration date added

--- app/src/main/java/com/yekmer/cardlib/sample/SampleActivity.java
+++ app/src/main/java/com/yekmer/cardlib/sample/SampleActivity.java
@@ -2,9 +2,11 @@ package com.yekmer.cardlib.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.yekmer.cardlib.CreditCardEditText;
 import com.yekmer.cardlib.OtherCardTextWatcher;
+import com.yekmer.cardlib.TwoDigitsCardTextWatcher;
 
 /**
  * Created by yekmer
@@ -12,14 +14,17 @@ import com.yekmer.cardlib.OtherCardTextWatcher;
 public class SampleActivity extends Activity {
 
     private CreditCardEditText creditCardEditText;
+    private EditText expirationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         creditCardEditText = (CreditCardEditText) findViewById(R.id.credit_card_edit_text);
+        expirationEditText = (EditText) findViewById(R.id.et_fragment_add_credit_card_expiry);
 
         creditCardEditText.addTextChangedListener(new OtherCardTextWatcher(creditCardEditText));
+        expirationEditText.addTextChangedListener(new TwoDigitsCardTextWatcher(expirationEditText));
 
     }
 }
